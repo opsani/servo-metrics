00463774009c4673ae516a455d4034d439d19165
Blake Watters <blake@opsani.com>
2021-05-27 09:59:06 -0700

    Update to ServoX v0.10.x dependency

--- servo_metrics.py
+++ servo_metrics.py
@@ -19,8 +19,8 @@ service.register(adjust_counter)
     description="Tracks metrics for servo events.",
     version="0.5.0",
     homepage="https://github.com/opsani/servox",
-    license=servo.License.APACHE2,
-    maturity=servo.Maturity.EXPERIMENTAL,
+    license=servo.License.apache2,
+    maturity=servo.Maturity.experimental,
 )
 class MetricsConnector(servo.BaseConnector):
     @servo.on_event()
@@ -32,12 +32,12 @@ class MetricsConnector(servo.BaseConnector):
     async def shutdown(self) -> None:
         await service.stop()
 
-    @servo.after_event(servo.Events.MEASURE)
+    @servo.after_event(servo.Events.measure)
     async def after_measure(self, results: List[servo.EventResult], **kwargs) -> None:
         self.logger.info("after_measure")
         measure_counter.inc({"servo": self.optimizer.id})
 
-    @servo.after_event(servo.Events.ADJUST)
+    @servo.after_event(servo.Events.adjust)
     async def after_adjust(self, results: List[servo.EventResult]) -> None:
         self.logger.info("after_adjust")
         adjust_counter.inc({"servo": self.optimizer.id})
